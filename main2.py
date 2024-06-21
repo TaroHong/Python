0d20b323b44594d0683a9243b920dfda54e97a2f
hgyk38 <121781376+TaroHong@users.noreply.github.com>
2024-06-21 22:47:29 +0900

    Update main2.py

--- main2.py
+++ main2.py
@@ -18,14 +18,14 @@ TELEGRAM_CHAT_ID = '7321984689'
 log_file_path = 'checked_posts.log'
 
 # 검색할 키워드 리스트
-keywords = ["에이토스", "마돈", "S5","벤지","라파","시스템식스","에스웍스","에스웍"]
+keywords = ["에이토스", "마돈", "S5", "벤지", "라파", "시스템식스", "에스웍스", "에스웍"]
 
 # 정규 표현식으로 키워드 패턴 생성
 keyword_pattern = re.compile('|'.join(keywords))
 
 # 텔레그램 메시지 전송 함수
-def send_telegram_message(message):
-    send_text = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={message}'
+def send_telegram_message(message, link):
+    send_text = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={message}\n{link}'
     response = requests.get(send_text)
     print(f"텔레그램 메시지 전송 상태: {response.status_code}")
     return response.json()
@@ -65,25 +65,23 @@ def crawl_site():
             if post_title in checked_posts:
                 continue  # 이미 확인된 게시물은 무시
             
-            post_link_element = title.find_parent('tr').find('a')
-            if post_link_element:
-                post_link = f"https://corearoadbike.com{post_link_element['href']}"
-                print(f"제목: {post_title}")
-                print(f"링크: {post_link}")
-                if keyword_pattern.search(post_title):
-                    print(f"키워드 발견: {post_title}")
-                    message = f"게시물 발견: {post_title}\n{post_link}"
-                    send_telegram_message(message)
-                    save_checked_post(post_title)  # 확인된 게시물을 로그 파일에 저장
-                    checked_posts.add(post_title)  # 확인된 게시물 추가
-            else:
-                print(f"게시물 링크를 찾을 수 없습니다: {post_title}")
+            # 게시물 번호 추출
+            post_no = title.find_parent('tr')['data-no']
+            
+            # 게시물 링크 생성
+            post_link = f"https://corearoadbike.com/board/board.php?g_id=recycle02&t_id=Menu01Top6&sort=wr_last+desc&no={post_no}"
+            
+            print(f"제목: {post_title}")
+            if keyword_pattern.search(post_title):
+                print(f"키워드 발견: {post_title}")
+                send_telegram_message(f"게시물 발견: {post_title}", post_link)
+                save_checked_post(post_title)  # 확인된 게시물을 로그 파일에 저장
+                checked_posts.add(post_title)  # 확인된 게시물 추가
     
     except HTTPError as e:
         print(f"HTTP Error: {e.code} - {e.reason}")
     except Exception as e:
         print(f"예외 발생: {e}")
-
 
 # 남은 시간 계산 및 표시 함수
 def display_remaining_time(next_run_time):
