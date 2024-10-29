d5329efb64c8a3e182c387614bac00229bb05abd
hgyk38 <121781376+TaroHong@users.noreply.github.com>
2024-10-29 14:33:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import schedule
 import time
 from datetime import datetime, timedelta
 import re
+from urllib.parse import quote  # URL 인코딩을 위한 모듈 추가
 
 # 크롤링할 URL
-target_url = "https://corearoadbike.com/board/board.php?t_id=Menu01Top6&category=%25ED%258C%2590%25EB%25A7%25A4&sort=wr_last+desc"
+target_url = "https://corearoadbike.com/board/board.php?t_id=Menu30Top6&category=%25ED%258C%2590%25EB%25A7%25A4&category2=%25EB%2594%2594%25EC%258A%25A4%25ED%2581%25AC&sort=wr_2+desc"
 
-# 텔레그램 봇 정보
+# 텔레그램 봇 정보/ 공공 토큰이라 가져가도 무관O
 TELEGRAM_BOT_TOKEN = '7272596527:AAE9de-Uw58CheN-ayHQoL1_MSPxur2O0b4'
 TELEGRAM_CHAT_ID = '7321984689'
 
@@ -18,7 +19,7 @@ TELEGRAM_CHAT_ID = '7321984689'
 log_file_path = 'checked_posts.log'
 
 # 검색할 키워드 리스트
-keywords = ["에이토스", "마돈", "S5","벤지","라파","시스템식스","에스웍스","에스웍"]
+keywords = ["하이퍼"]
 
 # 정규 표현식으로 키워드 패턴 생성
 keyword_pattern = re.compile('|'.join(keywords))
@@ -56,19 +57,37 @@ def crawl_site():
         print("사이트 응답 수신 완료")
         soup = BeautifulSoup(response, "html.parser")
         titles = soup.find_all('td', attrs={'class': 'list_title_B'})
+        contents = soup.find_all('td', attrs={'class': 'list_content_B'})
         
         if not titles:
             print("게시글을 찾을 수 없습니다.")
+            return
         
-        for title in titles:
-            post_title = title.text.strip()
+        for title, content in zip(titles, contents):
+            post_title = title.text.strip()  # 제목 추출
+            link_tag = title.find('a')  # <a> 태그 추출
+            post_link = link_tag['href'] if link_tag and 'href' in link_tag.attrs else ''  # 게시물 링크 추출
+            
+            # 절대 URL로 변환
+            if post_link.startswith('./'):
+                post_link = post_link[2:]  # './' 제거
+            
+            if post_link:
+                post_link = f"https://corearoadbike.com/board/{post_link}"  # 기본 URL 추가
+            
+            post_content = content.text.strip()  # 본문 내용 추출
+            
             if post_title in checked_posts:
                 continue  # 이미 확인된 게시물은 무시
             
             print(f"제목: {post_title}")
             if keyword_pattern.search(post_title):
                 print(f"키워드 발견: {post_title}")
-                send_telegram_message(f"게시물 발견: {post_title}")
+                print(f"링크: {post_link}")
+                # URL 인코딩
+                encoded_link = quote(post_link, safe=':/')  # URL 인코딩
+                message = f"게시물 발견: {post_title} \n-------------------------------------- \n내용: {post_content}\n --------------------------------------\n링크: {encoded_link}"
+                send_telegram_message(message)
                 save_checked_post(post_title)  # 확인된 게시물을 로그 파일에 저장
                 checked_posts.add(post_title)  # 확인된 게시물 추가
     
